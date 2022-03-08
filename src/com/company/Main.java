16f4e2a3ba7bda4c5fc1cb09cafe6ef5081914a7
VesiDaskalova <vessito@gmail.com>
2022-03-08 18:37:01 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    int number;
+    Scanner Scan = new Scanner(System.in);
+    System.out.println("Enter any number");
+    number = Scan.nextInt();
+    scan.close();
+    System.out.println(number);
+
+    }
+}
